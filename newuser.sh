4ad8e05f81138e5c37755c9dd23c6f597bf6ddaa
Sindre Thomassen <sindre.thomassen@safebase.no>
2023-12-01 08:48:55 +0100

    feat: initial commit

---
+++ newuser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "No user given"
+	exit 1
+fi
+
+for DAY in {01..24};
+do
+	mkdir $DAY/$1
+	touch $DAY/$1/.gitkeep
+done
+
+echo "Added folders for new user '$1'"
+
+exit 0
