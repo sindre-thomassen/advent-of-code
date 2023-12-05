8e152849a297989182d79edc4de60ae95cb02a10
Pucko321 <jonas.mela@gmail.com>
2023-12-05 15:30:28 +0100

    REFACTOR: default text

--- 01/Jonas/main.ts
+++ 01/Jonas/main.ts
@@ -10,9 +10,13 @@ import {CalibrationDecipherer} from "./typescript/calibration-decipherer";
 import {CalibrationDocument} from "./typescript/calibration-document";
 import {ScratchcardPointCounter} from '../../04/Jonas/scratchcard-point-counter';
 import {ScratchcardCollection} from '../../04/Jonas/scratchcard-collection';
+import {ChristmasPrinter} from './utility/christmas-printer';
+
+const christmasPrinter: ChristmasPrinter = new ChristmasPrinter();
+christmasPrinter.print({suggestedAnswer: 3, answer: 3});
 
 const day4: ScratchcardPointCounter = new ScratchcardPointCounter(ScratchcardCollection.REAL_DEAL);
-console.log(day4.getPointsPart1());
+// console.log(day4.getPointsPart1());
 
 const day1: CalibrationDecipherer = new CalibrationDecipherer(CalibrationDocument.REAL_DEAL);
 // console.log(day1.getDecipheredCalibrationDocumentPart1());
