e556404cd01b94357f778ba88e2b0d9c13706066
Pucko321 <jonas.mela@gmail.com>
2023-12-08 15:51:48 +0100

    FEAT: create array of left/right instructions

--- 08/Jonas/map-navigator.ts
+++ 08/Jonas/map-navigator.ts
@@ -1,6 +1,11 @@
 import {PouchOfMaps} from './pouch-of-maps';
 import {IChristmasPrinterInput} from '../../01/Jonas/utility/christmas-printer';
 
+export enum LeftRightInstruction {
+    LEFT = 'L',
+    RIGHT = 'R'
+}
+
 export class MapNavigator {
 
     private pouchOfMaps: PouchOfMaps;
@@ -10,10 +15,17 @@ export class MapNavigator {
     }
 
     public getStepsRequiredPart1(): IChristmasPrinterInput {
-
+        const leftRightInstructions: LeftRightInstruction[] = this.getLeftRightInstructions();
         return {
             suggestedAnswer: 0,
             answer: this.pouchOfMaps.answer
         };
+    }
+
+    private getLeftRightInstructions(): LeftRightInstruction[] {
+        return this.pouchOfMaps.value
+            .split('\n')[0]
+            .split('')
+            .map((letter: string) => letter === 'L' ? LeftRightInstruction.LEFT : LeftRightInstruction.RIGHT);
     }
 }
