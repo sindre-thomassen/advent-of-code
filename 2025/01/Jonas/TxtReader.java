e2d374887ec8a42a43c4cffaa9920db12a907ae6
Pucko321 <jonas.mela@gmail.com>
2025-12-01 15:18:53 +0100

    FEAT: txt reader

---
+++ 2025/01/Jonas/TxtReader.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TxtReader implements AutoCloseable {
+
+    private final BufferedReader reader;
+    private String nextLine;
+
+    public TxtReader(String path, String fileName) throws IOException {
+        this.reader = new BufferedReader(new FileReader(createFilePath(path, fileName)));
+        readNextLine();
+    }
+
+    public String getNextLine() throws Exception {
+        if (nextLine == null) {
+            return null;
+        }
+        String lineToReturn = nextLine;
+        readNextLine();
+        return lineToReturn;
+    }
+
+    public boolean hasNextLine() {
+        return nextLine != null;
+    }
+
+    private String createFilePath(String path, String fileName) {
+        return path + "/" + fileName + ".txt";
+    }
+
+    private void readNextLine() throws IOException {
+        nextLine = reader.readLine();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (reader != null) {
+            reader.close();
+        }
+    }
+}
