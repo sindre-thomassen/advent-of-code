53d988224a64564932b95248691214d68e75fc99
Pucko321 <jonas.mela@gmail.com>
2025-12-01 16:01:02 +0100

    FEAT: part1

--- 2025/01/Jonas/Main01.java
+++ 2025/01/Jonas/Main01.java
@@ -2,8 +2,45 @@ import util.christmas_printer.AdventOfCodeAnswer;
 
 public class Main01 {
     public static void main(String[] args) {
-        AdventOfCodeAnswer adventOfCodeAnswer = new AdventOfCodeAnswer("11 answer", "22 exp answer");
+//        test();
+//        part1();
+        part2();
+    }
+
+    public static void test() {
+        SafeDial safeDial = new SafeDial(50, 0, 99, 0);
+
+        try (RotationReader rotationReader = new RotationReader("2025/01/Jonas", "InputTestDay1")) {
+            while (rotationReader.hasNextLine()) {
+                RotationInstruction rotationInstruction = rotationReader.getNextRotation();
+                safeDial.rotate(rotationInstruction);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        AdventOfCodeAnswer adventOfCodeAnswer = new AdventOfCodeAnswer(String.valueOf(safeDial.getNumberCount()), "3");
         ChristmasPrinter2025 christmasPrinter = new ChristmasPrinter2025(adventOfCodeAnswer);
         christmasPrinter.print();
+    }
+
+    public static void part1() {
+        SafeDial safeDial = new SafeDial(50, 0, 99, 0);
+
+        try (RotationReader rotationReader = new RotationReader("2025/01/Jonas", "Input1Day1")) {
+            while (rotationReader.hasNextLine()) {
+                RotationInstruction rotationInstruction1 = rotationReader.getNextRotation();
+                safeDial.rotate(rotationInstruction1);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        AdventOfCodeAnswer adventOfCodeAnswer = new AdventOfCodeAnswer(String.valueOf(safeDial.getNumberCount()), "?");
+        ChristmasPrinter2025 christmasPrinter = new ChristmasPrinter2025(adventOfCodeAnswer);
+        christmasPrinter.print();
+    }
+
+    public static void part2() {
     }
 }
